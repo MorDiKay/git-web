4c66141c69dfa08c68c611c8a57fb56102fab069
Illia Mytchenko <illia_mytchenko@epam.com>
2021-06-03 16:02:17 +0300

    Practice 2: DB connection was implemented

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, ZooEvent, EventsHaveUsers
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'user_surname', 'user_email')
+    list_filter = ('user_name', 'user_email')
+
+
+class ZooEventAdmin(admin.ModelAdmin):
+    list_display = ['event_name']
+
+
+class EventsHaveUsersAdmin(admin.ModelAdmin):
+    list_display = ['user', 'tickets_count']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(ZooEvent, ZooEventAdmin)
+admin.site.register(EventsHaveUsers, EventsHaveUsersAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=20, verbose_name="User name", blank=False, default='name')
+    user_surname = models.CharField(max_length=30, verbose_name="User surname", blank=False, default='surname')
+    user_email = models.CharField(max_length=30, verbose_name="User email", blank=False)
+
+    def __str__(self):
+        return self.user_name
+
+
+class ZooEvent(models.Model):
+    event_name = models.CharField(max_length=30, verbose_name="Event name", blank=False, default='event')
+    event_date = models.DateField(verbose_name="Date", blank=False)
+    event_general_count = models.IntegerField(verbose_name="General count", blank=False)
+
+    def __str__(self):
+        return self.event_name
+
+
+class EventsHaveUsers(models.Model):
+    tickets_count = models.IntegerField(blank=False)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="User")
+    zoo_event = models.ForeignKey(ZooEvent, on_delete=models.PROTECT, verbose_name="ZooEvent")
